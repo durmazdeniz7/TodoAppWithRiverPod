4f4e9dfdafc3758cb7ad6c9289d5595fc4689a37
Deniz Durmaz <87248460+Denizdurmazz@users.noreply.github.com>
2022-03-01 14:38:16 +0300

    Riverpod

---
+++ lib/providers/todo_list_manager.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app_with_riverpod/models/todo_model.dart';
+import 'package:uuid/uuid.dart';
+
+class TodoListManager extends StateNotifier<List<TodoModel>> {
+  TodoListManager([List<TodoModel>? state]) : super(state ?? []);
+
+  void addTodo(String description) {
+    state = [
+      ...state,
+      TodoModel(id: const Uuid().v4(), description: description)
+    ];
+  }
+
+  void toggle(String id) {
+    state = [
+      for (var todo in state)
+        if (todo.id == id)
+          TodoModel(
+              id: id, description: todo.description, completed: !todo.completed)
+        else
+          todo,
+    ];
+  }
+
+  void edit({required String id, required String newDescription}) {
+    state = [
+      for (var todo in state)
+        if (todo.id == id)
+          TodoModel(
+              id: id, description: newDescription, completed: todo.completed)
+        else
+          todo,
+    ];
+  }
+
+  void remove(TodoModel todoModel){
+    state=state.where((element) => element.id!=todoModel.id).toList();
+  }
+}
